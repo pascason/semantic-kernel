aa7c5dffbc5f1cd21c6ac7bd8095dc50bd1e0dc2
pascason <roberto@alloymetrics.com>
2023-04-15 22:49:15 +0000

    Hello world and blank analytic function

--- hello-world.py
+++ hello-world.py
@@ -5,11 +5,8 @@ kernel = sk.create_kernel()
 
 # Prepare OpenAI backend using credentials stored in the `.env` file
 api_key, org_id = sk.openai_settings_from_dot_env()
-kernel.config.add_text_backend("dv", OpenAITextCompletion("text-davinci-003", api_key, org_id))
+kernel.config.add_text_backend("dv", OpenAITextCompletion("text-curie-001", api_key, org_id))
 
-# Alternative using Azure:
-# deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env()
-# kernel.config.add_text_backend("dv", AzureTextCompletion(deployment, endpoint, api_key))
 
 # Wrap your prompt in a function
 prompt = kernel.create_semantic_function("""
@@ -23,6 +20,29 @@ such orders would conflict with the First Law.
 does not conflict with the First or Second Law.
 
 Give me the TLDR in exactly 5 words.""")
+                                         
+                                         
 
 # Run your prompt
 print(prompt()) # => Robots must not harm humans.
+
+
+prompt = """{{$input}}
+Summarize the content above.
+"""
+
+summarize = kernel.create_semantic_function(prompt, max_tokens=2000, temperature=0.2, top_p=0.5)
+
+# note: using skills from the samples folder
+skills_directory = "samples/skills"
+
+funFunctions = kernel.import_semantic_skill_from_directory(skills_directory, "FunSkill")
+FunFunction = funFunctions["Joke"]
+result = FunFunction("A Neapolitan nerd walks into a bar")
+print(result)
+
+analyticFunctions = kernel.import_semantic_skill_from_directory(skills_directory, "AnalyticSkill")
+AnomalyFunction = analyticFunctions["FindTimelineAnomalies"]
+result = AnomalyFunction("Good morning to you")
+
+print(result)
